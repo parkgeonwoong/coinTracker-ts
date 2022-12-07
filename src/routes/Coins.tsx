329c83ea7679dbf6b2ede1b70931caa1057a06c5
parkgeonwoong <hk77kh@gmail.com>
2022-12-07 13:45:08 +0900

    Feat : Coins 페이지 기능추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,55 +1,66 @@
+/**
+ * @desc : Coins 페이지
+ * @route : /
+ */
+
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+// 가져올 api 인터페이스
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>🅲oin</Title>
       </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin>
-            <Link to={`/${coin.id}`}>{coin.name} 👉</Link>
-          </Coin>
-        ))}
-      </CoinList>
+      {/* 로딩 */}
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} 👉
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
     </Container>
   );
 }
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -75,15 +86,26 @@ const Coin = styled.li`
   border-radius: 15px;
 
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const Loading = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  height: 35px;
+  width: 35px;
+  margin-right: 10px;
 `;
 
 export default Coins;
