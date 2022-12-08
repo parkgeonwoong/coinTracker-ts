d59529f22d993e46b2d911dbf990ee4e9e8e7ca9
parkgeonwoong <hk77kh@gmail.com>
2022-12-08 16:08:02 +0900

    Feat : Coin 상세,가격 및 데이터타입 정의

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -3,8 +3,8 @@
  * @route : /:coinId
  */
 
-import { useState } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { CoinInterface } from "./Coins";
 
@@ -15,14 +15,89 @@ interface LocationState {
   };
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const { coinId } = useParams();
-  const location = useLocation();
+  const { coinId } = useParams(); // URL param에서 가져온 값
   const { state } = useLocation() as LocationState; // Link state에서 가져온 props
 
-  console.log(location.state.src);
-  console.log(state.coin);
+  // api 값 담는 곳
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+  // console.log(state.coin);
+
+  useEffect(() => {
+    (async () => {
+      // 상세정보 api
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      console.log(infoData);
+      // 가격 api
+      const priceData = await await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
