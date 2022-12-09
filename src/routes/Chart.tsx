3468794fb689a8f6927bad3529f227aa740a7aaf
parkgeonwoong <hk77kh@gmail.com>
2022-12-09 17:55:34 +0900

    Feat : ApexChart 적용해보기

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -6,18 +6,65 @@
 import { useQuery } from "@tanstack/react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
 
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading Chart..."
+      ) : (
+        <ApexChart
+          series={[
+            {
+              name: "Coin Chart",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          type="line"
+          options={{
+            theme: { mode: "dark" },
+            chart: { width: 500, height: 500, background: "transparent" },
+            yaxis: { show: false },
+            xaxis: {
+              categories:
+                data?.map((price) =>
+                  new Date(price.time_close).toLocaleDateString("en-US")
+                ) ?? [],
+              labels: { show: false },
+              axisTicks: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: { curve: "smooth" },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
